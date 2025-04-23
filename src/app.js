7b584486051005d20fc4f1fe252f640f7fe41158
juan camilo <oohankoo@hotmail.com>
2025-04-23 17:49:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
-window.onload=()=>{
-  
-  document.querySelector("#dominio-generado").innerHTML=generadorDominio();
-  
-}
-const generadorDominio=()=>{
-
-let pronoun = ['the', 'our', 'his'];
-let adj = ['great', 'big', 'amazing' ];
-let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
-let dominio = [ ".com", ".net", ".us", ".io"];
+const pronoun = ['the','our'];
+const adj = ['great', 'big' ];
+const noun = ['jogger','racoon'];
+const domin = ['.com','.net','.us','.io']
 
 
-let pronounGEN = Math.floor(Math.random()*pronoun.length);
-let adjGEN = Math.floor(Math.random()* adj.length);
-let nounGEN = Math.floor(Math.random()* noun.length);
-let dominioGEN = Math.floor(Math.random()* dominio.length);
-
-  return pronoun[pronounGEN] + adj[adjGEN] + noun[nounGEN] + dominio[dominioGEN];
+for (i = 0; i<pronoun.length; i++) {
+    for (j = 0; j<adj.length; j++){
+        for (k = 0; k<noun.length; k++){
+            for (r = 0; r<domin.length; r++){
+        console.log( pronoun[i] + adj[j] + noun[k] + domin[r]);
+      }
+    }
+  }
 }
